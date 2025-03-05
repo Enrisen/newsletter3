39d9552e631bb077d74e9ca72b6f822b479a7405
Mickali Garbutt <2020151994@ub.edu.bz>
2025-03-05 02:02:49 +0000

    Dependency Injection 4/4

---
+++ cmd/web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+type application struct {
+	logger *slog.Logger
+}
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{
+		logger: logger,
+	}
+	err := app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
